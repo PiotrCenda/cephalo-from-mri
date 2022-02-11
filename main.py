3a273302c13a15c2d101671bb084957cf65c8d10
piotr_cenda <piotr.cenda222@gmail.com>
2022-02-11 17:29:46 +0100

    import cleanup

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 
 from data_loader import read_data_from_folder
-from data_manipulation import save_tif, timer_block
+from data_manipulation import timer_block
 from data_plotting import plot_3d
 from interpolation import cephalo, interpolate
 
